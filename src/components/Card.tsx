2f3880354c91d3ba5218323f34791d49e099bb86
Siris <40269790+siriscmv@users.noreply.github.com>
2023-06-26 18:23:50 +0530

    feat: bold text in card comp

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -71,8 +71,12 @@ export default function Card(props: CardProps) {
       <img className={styles.img} src={props.image} alt={props.title} />
       <div className={styles.content}>
         <div className={styles.header}>
-          <span className={styles.title}>{props.title}</span>
-          <span>Price: ₹{(props.price * (qty ?? 1)).toFixed(2)}</span>
+          <span className={styles.title}>
+            <b>{props.title}</b>
+          </span>
+          <span>
+            Price: <b>₹{(props.price * (qty ?? 1)).toFixed(2)}</b>
+          </span>
         </div>
         <span className={styles.description}>{props.description}</span>
         {state.cart.some((c) => c.id === props.id) ? (
