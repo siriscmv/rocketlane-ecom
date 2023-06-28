ba33d31f030784a32419079006bedbfbec8626c0
Siris <40269790+siriscmv@users.noreply.github.com>
2023-06-28 11:04:53 +0530

    fix: error page

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -4,13 +4,16 @@ import styles from "./Page.module.css";
 export default function Error() {
   const [errorMessage, setErrorMessage] = useState<string>("");
   useEffect(() => {
-    setErrorMessage(window.location.search);
+    const msg =
+      new window.URL(window.location.href).searchParams.get("msg") ??
+      "Something went wrong";
+    setErrorMessage(msg);
   }, []);
 
   return (
     <div className={styles.container}>
       <div className={styles.main}>
-        <h2>{errorMessage}</h2>
+        <h2 className={styles.danger}>{errorMessage}</h2>
       </div>
     </div>
   );
