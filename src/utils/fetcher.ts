769503cec833afa689085ef4cd300947d3ebf40e
Siris <40269790+siriscmv@users.noreply.github.com>
2023-06-30 11:28:24 +0530

    chore: incl new file

---
+++ src/utils/fetcher.ts
@@ -0,0 +1,28 @@
+export default async function fetcher(
+  path: string,
+  method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE",
+  body?: Record<string, unknown>
+) {
+  const opts: RequestInit = { method: method ?? "GET" };
+  opts.headers = {
+    "ngrok-skip-browser-warning": "69420",
+  };
+  if (body) {
+    opts.body = JSON.stringify(body);
+    opts.headers["Content-Type"] = "application/json";
+  }
+
+  if (!process.env.REACT_APP_BASE_API_URL)
+    throw new Error("REACT_APP_BASE_API_URL is not set");
+
+  const result = await window.fetch(
+    `${process.env.REACT_APP_BASE_API_URL}${path}`,
+    opts
+  );
+
+  if (!result.ok) {
+    window.location.href = `/error?msg=${`Error, status code ${result.status}`}`;
+    return null;
+  }
+  return await result.json();
+}
